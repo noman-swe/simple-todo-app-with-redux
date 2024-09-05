71af45478a3ee79ffd0f6ce33e977c1cd1e4be3e
noman-swe <noman.swe130@gmail.com>
2024-09-05 23:54:17 +0600

    [initializing the project]

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import bgImage from "./assets/images/bg.jpg";
+function App() {
+  return (
+    <div
+      className="app h-screen w-screen mx-0 px-0"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      
+    </div>
+  );
+}
+
+export default App;
